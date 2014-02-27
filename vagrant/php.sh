c5d135476014a2db5b7608deb8e767173e0cb2f1
Rudy Lee <rudylee1988@gmail.com>
2014-02-28 10:27:33 +1100

    Update the php.sh vagrant provision file to install latest tmux and yeoman

--- vagrant/php.sh
+++ vagrant/php.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Install Required Packages
+apt-get install -y software-properties-common
+apt-get install -y python-software-properties
+add-apt-repository -y ppa:pi-rho/dev
 apt-get update
 apt-get install -y vim
 apt-get install -y tmux
@@ -8,6 +11,13 @@ apt-get install -y git
 apt-get install -y subversion
 apt-get install -y fontconfig
 apt-get install -y curl
+apt-get install -y libcurl3 libcurl3-gnutls libcurl4-openssl-dev
+
+# Set the git color
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
 
 # Set up Vim and Tmux
 export HOME="/home/vagrant"
@@ -22,6 +32,7 @@ ln -sf $HOME/dotfiles/vimrc $HOME/.vimrc
 mkdir $HOME/.vim/ftplugin
 ln -sf $HOME/dotfiles/ftplugin/php.vim $HOME/.vim/ftplugin/php.vim
 ln -sf $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/gitconfig $HOME/.gitconfig
 mv $HOME/molokai/colors $HOME/.vim
 vim +BundleInstall +qall
 
@@ -43,3 +54,15 @@ if ! grep -q "stty start undef" "$HOME/.bashrc"; then
 fi
 
 # Install PHP, PHPMyAdmin and MySQL
+
+# Install NodeJS, NPM and Yeoman
+if ! type "node" > /dev/null; then
+    export SRC="/usr/local/src"
+    cd $SRC
+    wget http://nodejs.org/dist/v0.10.22/node-v0.10.22-linux-x86.tar.gz -O node.tar.gz
+    tar -xvf node.tar.gz && mv node-v0.10.22-linux-x86 nodejs
+    rm -Rf $SRC/node.tar.gz
+    ln -sf $SRC/nodejs/bin/node /usr/local/bin/node
+    ln -sf $SRC/nodejs/bin/npm /usr/local/bin/npm
+    npm install -g yo
+fi
