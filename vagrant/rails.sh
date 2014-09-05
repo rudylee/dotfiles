7a68c7e67c0a08339d00c2afcf43f0791329748b
Rudy <rudylee1988@gmail.com>
2014-09-05 11:24:14 +1000

    Upgrade NodeJS version

--- vagrant/rails.sh
+++ vagrant/rails.sh
@@ -66,8 +66,8 @@ fi
 if ! type "node" > /dev/null; then
     export SRC="/usr/local/src"
     cd $SRC
-    wget http://nodejs.org/dist/v0.10.22/node-v0.10.22-linux-x86.tar.gz -O node.tar.gz
-    tar -xvf node.tar.gz && mv node-v0.10.22-linux-x86 nodejs
+    wget http://nodejs.org/dist/v0.10.31/node-v0.10.31-linux-x86.tar.gz -O node.tar.gz
+    tar -xvf node.tar.gz && mv node-v0.10.31-linux-x86 nodejs
     rm -Rf $SRC/node.tar.gz
     ln -sf $SRC/nodejs/bin/node /usr/local/bin/node
     ln -sf $SRC/nodejs/bin/npm /usr/local/bin/npm
